bc8ce75dcee12daa88d489eec79d41d94d3292a2
Nguyễn Thanh Trí <trint4@vng.com.vn>
2019-01-25 14:15:44 +0700

    create project (cont)

---
+++ Podfile
@@ -0,0 +1,9 @@
+platform :ios, '9.0'
+
+use_frameworks!
+
+target 'SRS' do
+  pod 'AFNetworking', '~> 3.0'
+  pod 'RxSwift', '~> 4.0'
+  pod 'RxCocoa', '~> 4.0'
+end
---
+++ SRS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SRS
+//
+//  Created by Nguyễn Thanh Trí on 1/25/19.
+//  Copyright © 2019 TriNguyen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
